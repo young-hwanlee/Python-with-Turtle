aa826bb5a9e40cb4aeaae488df74d9b0118c51a1
Young-hwan Lee <zerolyh@gmail.com>
2021-03-15 05:49:35 +0000

    Add

---
+++ 5_circle.py
@@ -0,0 +1,13 @@
+import turtle
+
+r = 10
+n = 100
+
+while r <= n:
+    turtle.circle(r)
+    r = r + 10
+
+r = -10
+while r >= -100:
+    turtle.circle(r)
+    r = r - 10
--- main.py
+++ main.py
@@ -72,44 +72,27 @@
 # while count <= 4:
 #     print("Run", count)
 #     count = count + 1
-
-import turtle
-# # Rectangle with loops
-# count = 0
-# while count < 4:
-#     turtle.forward(100)
-#     turtle.right(90)
-#     count = count + 1
-
-# # Polygon with loops
-# n = 5
-# n = 6
-# n = 7
-n = 20
-
-count = 0
-while count < n:
-    # turtle.forward(100)
-    turtle.forward(200/n)
-    turtle.right(360/n)
-    count = count + 1
-
-
 # --------------------------------
 
+
+
 # 6. Nested if/Loop
+
 # --------------------------------
 
 
 # 7. For Loop
+
 # --------------------------------
 
 
 # 8. Nest for Loop
+
 # --------------------------------
 
 
 # 9. python LIST!
+
 # --------------------------------
 
 
