75e11b69cb0c710b9c6a11d2af9d81455288411b
Young-hwan Lee <zerolyh@gmail.com>
2021-03-15 06:08:08 +0000

    Add

---
+++ 5_dots.py
@@ -0,0 +1,30 @@
+import turtle
+
+# turtle.dot(3)
+# turtle.dot(5)
+
+# turtle.goto(10, 100)
+# turtle.dot(5)
+
+# turtle.up()
+# turtle.goto(10, 100)
+# turtle.down()
+# turtle.dot(5)
+
+# y = 0
+# x = 0
+# while x < 200:
+#     turtle.up()
+#     turtle.goto(x, y)
+#     turtle.down()
+#     turtle.dot(5)
+#     x = x + 20
+
+y = 0
+x = 0
+while x < 200:
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.dot(5)
+    x = x + 20
