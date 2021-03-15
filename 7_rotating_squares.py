fd89c7ec4c9284961295a2dd6e93bf02c5f1644c
Young-hwan Lee <zerolyh@gmail.com>
2021-03-15 07:40:11 +0000

    Add

---
+++ 7_rotating_squares.py
@@ -0,0 +1,14 @@
+import turtle
+
+for i in range(0, 200, 2):
+    turtle.forward(i)
+    turtle.right(91)
+
+turtle.up()
+turtle.home()
+turtle.down()
+turtle.right(45)
+
+for i in range(0, 200, 2):
+    turtle.forward(i)
+    turtle.right(89)
