a655dffdf58655effc0fc2e17b4ca005adf56696
Replit user <replituser@example.com>
2021-03-15 04:36:22 +0000

    commitgit add .

---
+++ heart.py
@@ -0,0 +1,44 @@
+# Import turtle package
+import turtle
+
+# Set the fill color to red
+turtle.fillcolor('red')
+
+# Start filling the color
+turtle.begin_fill()
+
+# Draw the left line
+turtle.left(140)
+turtle.forward(113)
+
+# Draw the left curve
+turtle.circle(-57, 200)
+turtle.left(120)
+
+# Draw the right curve
+turtle.circle(-57, 200)
+
+# Draw the right line
+turtle.forward(112)
+
+# Ending the filling of the color
+turtle.end_fill()
+
+# Move turtle to air
+turtle.up()
+
+# Move turtle to a given position
+turtle.goto(-60, 95)
+
+# Move the turtle to the ground
+turtle.down()
+
+# Set the text color to lightgreen
+turtle.color('lightgreen')
+
+# Write the specified text in
+# specified font style and size
+turtle.write("I Love YOU", font=("Verdana", 15, "bold"))
+
+# To hide turtle
+turtle.ht()
---
+++ main.py
@@ -0,0 +1,52 @@
+# # 3. Turtle Graphics
+# # Import turtle package
+# import turtle
+
+# turtle.circle(100)
+# turtle.circle(-100)
+
+
+
+# 4. if condition (조건문)
+# Import turtle package
+import turtle
+
+# Draw a line and turn 90
+turtle.forward(100)
+turtle.right(90)
+
+# Draw a line and turn 90
+turtle.forward(100)
+turtle.right(90)
+
+# Draw a line and turn 90
+turtle.forward(100)
+turtle.right(90)
+
+# Draw a line and turn 90
+turtle.forward(100)
+turtle.right(90)
+
+
+
+# 5. Loop
+
+
+
+# 6. Nested if/Loop
+
+
+
+# 7. For Loop
+
+
+
+# 8. Nest for Loop
+
+
+
+# 9. python LIST!
+
+
+
+# 10. for+list and break&continue
