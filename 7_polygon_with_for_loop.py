b79ab2eedb3394521096230fc8367c700d4f18a4
Young-hwan Lee <zerolyh@gmail.com>
2021-03-15 07:29:27 +0000

    Add

--- 7_polygon_with_for_loop.py
+++ 7_polygon_with_for_loop.py
@@ -1,5 +1,11 @@
 import turtle
 
-for i in range(4):
+# for i in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+
+# n = 5
+n = 7
+for i in range(n):
     turtle.forward(100)
-    turtle.right(90)
+    turtle.right(360/n)
---
+++ 7_star.py
@@ -0,0 +1,5 @@
+import turtle
+
+for _ in range(5):
+    turtle.forward(100)
+    turtle.right(144)
